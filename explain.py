67aa167e64efdd7ebcd168cb31b31f85a70b6999
Thomas J. Fan <thomasjpfan@gmail.com>
2020-09-14 18:37:07 -0400

    BLD Init commit

---
+++ explain.py
@@ -0,0 +1,121 @@
+import joblib
+import pandas as pd
+import shap
+from anchor.anchor_tabular import AnchorTabularExplainer
+from pathlib import Path
+import numpy as np
+import streamlit as st
+import streamlit.components.v1 as componenets
+
+categorical_columns = ['island', 'gender']
+numerical_columns = ['culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm', 'body_mass_g']
+
+
+@st.cache
+def get_metadata():
+    metadata = {}
+    penguins = pd.read_csv("penguins.csv",
+                           dtype={"species": 'category',
+                                  "island": 'category',
+                                  "gender": 'category'})
+
+    for cat_col in categorical_columns:
+        metadata[f'{cat_col}_labels'] = np.unique(penguins[cat_col]).tolist()
+
+    for num_col in numerical_columns:
+        metadata[f"{num_col}_min"] = penguins[num_col].min()
+        metadata[f"{num_col}_max"] = penguins[num_col].max()
+
+    metadata['class_labels'] = np.unique(penguins['species']).tolist()
+    metadata['all_columns'] = penguins.columns.drop('species').tolist()
+    return metadata
+
+
+@st.cache
+def get_clf():
+    return joblib.load('penguin_clf.joblib')
+
+
+metadata = get_metadata()
+user_input = {}
+
+for col in categorical_columns:
+    user_input[col] = [st.sidebar.radio(f"Select {col}", metadata[f"{col}_labels"])]
+
+
+step_map = {
+   'culmen_length_mm': 0.5,
+   'culmen_depth_mm': 0.5,
+   'flipper_length_mm': 5.0,
+   'body_mass_g': 100.0
+}
+for col in numerical_columns:
+    user_input[col] = [st.sidebar.number_input(f"Select {col}",
+                                               min_value=metadata[f"{col}_min"],
+                                               max_value=metadata[f"{col}_max"],
+                                               step=step_map[col])]
+
+clf = joblib.load("penguin_clf.joblib")
+class_names = metadata['class_labels']
+all_columns = metadata['all_columns']
+
+user_df = pd.DataFrame(user_input, columns=all_columns)
+
+prediction = clf.predict(user_df)[0]
+class_prediction = class_names[prediction]
+st.sidebar.write(f"## Prediction: {class_prediction}")
+proba = clf.predict_proba(user_df)
+proba_df = pd.DataFrame(proba, columns=class_names)
+st.sidebar.write(proba_df)
+
+X_encoded = clf[:-1].transform(user_df)
+X_encoded_df = pd.DataFrame(X_encoded, columns=all_columns)
+explainer = shap.TreeExplainer(clf[-1])
+shap_values = explainer.shap_values(X_encoded[[0], :], check_additivity=False)
+
+st.write(f"## Explanation for Predicting: **{class_prediction}**")
+st.subheader("SHAP values")
+
+@st.cache
+def bundle_js():
+    bundle_path = Path(shap.__file__).parent / 'plots' / 'resources' / "bundle.js"
+    with bundle_path.open('r') as f:
+        bundle_data = f.read()
+        bundle_js = f"<script charset='utf-8'>{bundle_data}</script>"
+    return bundle_js
+
+
+shape_js = bundle_js()
+
+values1 = shap.force_plot(explainer.expected_value[0],
+                          shap_values[0][0,:],
+                          X_encoded_df.iloc[0,:], out_names=class_names[0])
+values2 = shap.force_plot(explainer.expected_value[1],
+                          shap_values[1][0,:],
+                          X_encoded_df.iloc[0,:], out_names=class_names[1])
+values3 = shap.force_plot(explainer.expected_value[2],
+                          shap_values[2][0,:],
+                          X_encoded_df.iloc[0,:], out_names=class_names[2])
+componenets.html(f"{shape_js}{values1._repr_html_()}{values2._repr_html_()}"
+                 f"{values3._repr_html_()}", height=420)
+
+st.header("Anchors")
+
+
+@st.cache
+def get_anchor_explainder():
+    penguins = pd.read_csv("penguins.csv")
+    X_encoded_all = clf[:-1].transform(penguins[all_columns])
+    anchor_explainer = AnchorTabularExplainer(
+        class_names, all_columns, X_encoded_all,
+        categorical_names={0: metadata['island_labels'],
+                           1: metadata['gender_labels']})
+    return anchor_explainer
+
+
+anchor_explainer = get_anchor_explainder()
+exp = anchor_explainer.explain_instance(X_encoded[0, :],
+                                        clf[-1].predict, threshold=0.95)
+
+exp_html = exp.as_html()
+componenets.html(exp_html, height=1000)
---
+++ intro.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title("Show me some penguins!")
+
+species = st.radio("Select a penguin", ["adelie", "gentoo", "chinstrap"])
+
+st.image(f"{species}.jpg", use_column_width=True)
+
+species_to_wiki = {
+    'adelie': 'https://en.wikipedia.org/wiki/Adélie_penguin',
+    'gentoo': 'https://en.wikipedia.org/wiki/Gentoo_penguin',
+    'chinstrap': 'https://en.wikipedia.org/wiki/Chinstrap_penguin'
+}
+
+st.markdown(f"### Learn more about {species} penguins at "
+            "[wikipedia]({species_to_wiki[species]})!")
+
+st.title("EDA for penguins dataset!")
+
+penguins = pd.read_csv("penguins.csv")
+
+st.header("Lets see the dataset")
+penguins
+
+st.header("How much of each species are in our dataset?")
+
+fig, ax = plt.subplots()
+penguins['species'].value_counts().plot.bar(ax=ax,
+                                            color=['blue', 'red', 'green'])
+
+fig
+
+st.header("Can flipper length be used to distingushed between species?")
+
+flipper_fig = sns.displot(penguins, x='flipper_length_mm', hue='species',
+                          kind='kde')
+
+flipper_fig.fig
+
+st.header("Is the culmen useful for classifying species?")
+
+scat = px.scatter(penguins,
+                  x="culmen_length_mm",
+                  y="culmen_depth_mm",
+                  marginal_y="violin",
+                  marginal_x='box',
+                  color='species')
+scat
+
+st.header("How is the body mass for each species?")
+
+box = px.box(penguins, x="species", y="body_mass_g", color="gender")
+box
