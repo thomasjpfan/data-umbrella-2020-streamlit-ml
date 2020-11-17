c876eab45799b5c3263910f8dd755265882bf971
Thomas J. Fan <thomasjpfan@gmail.com>
2020-11-16 21:16:00 -0500

    ENH Remove caching

--- explain.py
+++ explain.py
@@ -8,49 +8,35 @@ import streamlit as st
 import streamlit.components.v1 as componenets
 
 categorical_columns = ['island', 'gender']
-numerical_columns = ['culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm', 'body_mass_g']
+numerical_columns = [
+    'culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm', 'body_mass_g'
+]
 
-
-@st.cache
-def get_penguins():
-    return pd.read_csv("penguins.csv",
+penguins = pd.read_csv("penguins.csv",
                        dtype={
                            "species": 'category',
                            "island": 'category',
                            "gender": 'category'
                        })
 
+metadata = {}
 
-penguins = get_penguins()
+for cat_col in categorical_columns:
+    metadata[f'{cat_col}_labels'] = np.unique(penguins[cat_col]).tolist()
 
+for num_col in numerical_columns:
+    metadata[f"{num_col}_min"] = penguins[num_col].min()
+    metadata[f"{num_col}_max"] = penguins[num_col].max()
 
-@st.cache
-def get_metadata():
-    metadata = {}
+metadata['class_labels'] = np.unique(penguins['species']).tolist()
+metadata['all_columns'] = penguins.columns.drop('species').tolist()
 
-    for cat_col in categorical_columns:
-        metadata[f'{cat_col}_labels'] = np.unique(penguins[cat_col]).tolist()
-
-    for num_col in numerical_columns:
-        metadata[f"{num_col}_min"] = penguins[num_col].min()
-        metadata[f"{num_col}_max"] = penguins[num_col].max()
-
-    metadata['class_labels'] = np.unique(penguins['species']).tolist()
-    metadata['all_columns'] = penguins.columns.drop('species').tolist()
-    return metadata
-
-
-@st.cache
-def get_clf():
-    return joblib.load('penguin_clf.joblib')
-
-
-metadata = get_metadata()
 user_input = {}
 
 for col in categorical_columns:
-    user_input[col] = [st.sidebar.radio(f"Select {col}", metadata[f"{col}_labels"])]
-
+    user_input[col] = [
+        st.sidebar.radio(f"Select {col}", metadata[f"{col}_labels"])
+    ]
 
 step_map = {
     'culmen_length_mm': 5.0,
@@ -59,19 +45,21 @@ step_map = {
     'body_mass_g': 200.0
 }
 for col in numerical_columns:
-    user_input[col] = [st.sidebar.number_input(f"Select {col}",
-                                               min_value=metadata[f"{col}_min"],
-                                               max_value=metadata[f"{col}_max"],
-                                               step=step_map[col])]
+    user_input[col] = [
+        st.sidebar.number_input(f"Select {col}",
+                                min_value=metadata[f"{col}_min"],
+                                max_value=metadata[f"{col}_max"],
+                                step=step_map[col])
+    ]
 
 clf = joblib.load("penguin_clf.joblib")
 class_names = metadata['class_labels']
 all_columns = metadata['all_columns']
 
 user_df = pd.DataFrame(user_input, columns=all_columns)
-
 prediction = clf.predict(user_df)[0]
 class_prediction = class_names[prediction]
+
 st.sidebar.write(f"## Prediction: {class_prediction}")
 proba = clf.predict_proba(user_df)
 proba_df = pd.DataFrame(proba, columns=class_names)
@@ -86,9 +74,11 @@ shap_values = explainer.shap_values(X_encoded[[0], :], check_additivity=False)
 st.write(f"## Explanation for Predicting: **{class_prediction}**")
 st.subheader("SHAP values")
 
+
 @st.cache
 def bundle_js():
-    bundle_path = Path(shap.__file__).parent / 'plots' / 'resources' / "bundle.js"
+    bundle_path = Path(
+        shap.__file__).parent / 'plots' / 'resources' / "bundle.js"
     with bundle_path.open('r') as f:
         bundle_data = f.read()
         bundle_js = f"<script charset='utf-8'>{bundle_data}</script>"
@@ -134,7 +124,8 @@ def get_anchor_explainder():
 
 anchor_explainer = get_anchor_explainder()
 exp = anchor_explainer.explain_instance(X_encoded[0, :],
-                                        clf[-1].predict, threshold=0.95)
+                                        clf[-1].predict,
+                                        threshold=0.95)
 
 exp_html = exp.as_html()
 componenets.html(exp_html, height=700)
